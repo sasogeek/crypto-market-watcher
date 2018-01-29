aa3eb3834fd5718d01b6c966e5c813b98cc1c92d
Samuel Sowah <samuel@official-notebook.com>
2018-01-29 16:30:38 +0000

    .

--- client/main.js
+++ client/main.js
@@ -3,20 +3,16 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+Template.buying.helpers({
+    buying: ()=>{
+      return Ticker.findOne({'name': 'bitcoin'}).ticker.ticker.buy
+    },
+
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.selling.helpers({
+    selling() {
+      return Ticker.findOne({'name': 'bitcoin'}).ticker.ticker.sell
+    },
 });
--- server/main.js
+++ server/main.js
@@ -2,21 +2,13 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
-    setInterval(()=>{}, 1000);
-    setInterval(()=>{console.log(Ticker.findAll())}, 1000)
+    setInterval(Meteor.bindEnvironment(function(){
+        let ticker = HTTP.get('https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd');
+        Ticker.upsert({'name': 'bitcoin'}, {$set: {'name': 'bitcoin', 'ticker': JSON.parse(ticker.content)}})
+    }), 500);
 });
 
 
 Meteor.methods({
-    get_ticker: () => {
-        let ticker = HTTP.get('https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd');
-        return ticker.content;
-    },
 
-    update: () => {
-        Meteor.call('get_ticker', ()=> {
-            Ticker.upsert({'name': 'bitcoin'}, {$set: {'name': 'bitcoin', 'ticker': ticker.ticker, 'date': ticker.date}})
-        });
-
-    }
 });
